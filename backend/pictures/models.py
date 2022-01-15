9cf9188cd527debf04fdfedf9cab09e567eb80a0
sewonK <cutecat1128@naver.com>
2022-01-15 14:08:01 +0900

    [Feat] Create Pictrue model

---
+++ backend/pictures/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+from uuid import uuid4
+import os
+
+# Create your models here.
+class Picture(models.Model):
+    def date_upload_to(instance, filename):
+      uuid_name = uuid4().hex
+      extension = os.path.splitext(filename)[-1].lower()
+      return '/'.join([
+        'Picture',
+        timezone.now().strftime('%Y/%m/%d'),
+        uuid_name + extension,
+      ])
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField(blank=True)
+    description = models.TextField(blank=True)
+    author = models.TextField(blank=True)
+    picture = models.ImageField(upload_to=date_upload_to)
+    exchange_user = models.ManyToManyField(User, blank=True, related_name='exchange_pic', through='PicturePrivacy')
+
+    def __str__(self):
+        return f'ownername = {self.user.username}, author = {self.author}, title = {self.title}, description = {self.description}'
+
+class PicturePrivacy(models.Model):
+    picture = models.ForeignKey(Picture, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    
