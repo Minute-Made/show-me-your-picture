b3839b06f5dcdf663994e7e39ab8a3a1117c80b9
yilin-kim <irenek91@korea.ac.kr>
2022-01-15 16:59:41 +0900

    내 사진첩

--- frontend/src/components/auth/style.js
+++ frontend/src/components/auth/style.js
@@ -1,152 +1,143 @@
-import styled, { css, keyframes} from "styled-components";
-
-export const BG = styled.div`
-  width: 100%;
-  background-repeat : no-repeat;
-  background-size : contain;
-  opacity: 50%;
-  backgroundImage : url('../../assets/img/SplashBG.png');
-`
+import styled, { css, keyframes } from "styled-components";
 
 export const InputIcon = styled.i`
-  font-size:1.5rem;
+  font-size: 1.5rem;
   margin-right: 4px;
-
-`
+`;
 export const YB = styled.div`
   height: 10vh;
-`
+`;
 export const Title = styled.p`
   margin-bottom: 50px;
   // font-family: 'Spoqa Hans Sans Neo',sans-serif;
-  font-family: 'NeoDunggeunmo Pro';
+  font-family: "NeoDunggeunmo Pro";
   font-weight: bold;
   font-size: 2rem;
-  
-`
+`;
 export const SubTitle = styled.div`
   width: 88%;
-  font-family: 'Spoqa Hans Sans Neo',sans-serif;
+  font-family: "Spoqa Hans Sans Neo", sans-serif;
   font-weight: 700;
-  font-size 1.125rem;
+  font-size: 1.125rem;
   text-align: left;
   margin-top: 1rem;
   margin-bottom: 1rem;
-  color:#404D61
-`
+  color: #404d61;
+`;
 export const Container = styled.div`
   width: 100%;
   max-width: 375px;
   padding: 0 30px 0 30px;
   margin: 0 auto;
-  display:flex;
+  display: flex;
   justify-content: center;
   flex-direction: column;
-
-`
+`;
 export const Circle = styled.div`
   width: 12px;
   height: 44px;
   margin-top: 10px;
   // background-color: #fff;
-  display:flex;
-  cursor:pointer;
+  display: flex;
+  cursor: pointer;
   align-items: center;
   justify-content: center;
   border-radius: 40px;
-`
+`;
 export const InputWrapper = styled.div`
   width: 100%;
   height: 56px;
-  display:flex;
+  display: flex;
   justify-content: center;
   align-items: center;
   margin-bottom: 1rem;
-`
+`;
 
 export const ButtonWrapper = styled.div`
   width: 100%;
-  display:flex;
+  display: flex;
   justify-content: center;
   flex-direction: column;
   align-items: center;
-`
+`;
 export const InputID = styled.input`
   width: 100%;
   font-size: 1rem;
   height: 100%;
-  font-family: 'Spoqa Han Sans Neo', 'sans-serif';
+  font-family: "Spoqa Han Sans Neo", "sans-serif";
   border-style: none;
   border-radius: 20px;
   padding-left: 20px;
-  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.1)) drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.1));
-`
-export const InputPW = styled(InputID)`
-`
+  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.1))
+    drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.1));
+`;
+export const InputPW = styled(InputID)``;
 export const Input = styled.input`
   width: 88%;
   line-height: 40px;
-  border: 1px solid #E1E3E6;
+  border: 1px solid #e1e3e6;
   box-sizing: border-box;
   border-radius: 8px;
   font-family: "Spoqa Hans Sans Neo";
   margin-bottom: 12px;
-`
+`;
 export const Select = styled.select`
   width: 88%;
   height: 40px;
-  font-family: 'Spoqa Han Sans Neo', 'sans-serif';
-  border: 1px solid #E1E3E6;
+  font-family: "Spoqa Han Sans Neo", "sans-serif";
+  border: 1px solid #e1e3e6;
   border-radius: 8px;
   color: grey;
-`
+`;
 export const Button = styled.button`
   width: 70%;
   height: 56px;
-  display:flex;
+  display: flex;
   align-items: center;
   background-color: white;
   justify-content: center;
   // font-family: 'Spoqa Han Sans Neo', 'sans-serif';
-  font-family: 'NeoDunggeunmo Pro';
-  font-size:1.125rem;
+  font-family: "NeoDunggeunmo Pro";
+  font-size: 1.125rem;
   font-weight: 700;
-  cursor:pointer;
+  cursor: pointer;
   border-radius: 80px;
   background: rgba(34, 34, 34, 0.5);
-  color:white;
-  &:hover{
+  color: white;
+  &:hover {
     background-color: #eee;
-    color:rgba(34, 34, 34, 0.5);
+    color: rgba(34, 34, 34, 0.5);
     transition: all ease 0.5s;
   }
   border-style: none;
-  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.1)) drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.1));
-`
+  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.1))
+    drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.1));
+`;
 export const Button1 = styled.div`
   width: 70%;
   height: 56px;
-  display:flex;
+  display: flex;
   align-items: center;
   background-color: white;
   justify-content: center;
   // font-family: 'Spoqa Han Sans Neo', 'sans-serif';
-  font-family: 'NeoDunggeunmo Pro';
-  font-size:1.125rem;
+  font-family: "NeoDunggeunmo Pro";
+  font-size: 1.125rem;
   font-weight: 700;
-  cursor:pointer;
+  cursor: pointer;
   border-radius: 80px;
-  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.1)) drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.1));
-`
-export const Button2= styled(Button1)`
+  filter: drop-shadow(0px 2px 4px rgba(0, 0, 0, 0.1))
+    drop-shadow(0px 4px 20px rgba(0, 0, 0, 0.1));
+`;
+export const Button2 = styled(Button1)`
   background: rgba(34, 34, 34, 0.5);
-  color:white;
-  &:hover{
+  color: white;
+  &:hover {
     background-color: #eee;
     transition: all ease 0.5s;
-    color:rgba(34, 34, 34, 0.5);
+    color: rgba(34, 34, 34, 0.5);
   }
-`
+`;
 const slideUp = keyframes`
     0% {
       opacity: 0;
@@ -161,13 +152,13 @@ const slideUp = keyframes`
     }
   `;
 
-export const TitleImg= styled.img`
+export const TitleImg = styled.img`
   animation-duration: 2s;
   animation-name: ${slideUp};
   // transition: all ease 1s;
   // transform:  ;
   width: 30%;
-  margin-top:12vh;
-  margin-bottom:12vh;
+  margin-top: 12vh;
+  margin-bottom: 12vh;
   filter: drop-shadow(2px 2px 4px rgba(0, 0, 0, 0.25));
-`
+`;
--- frontend/src/pages/Router.js
+++ frontend/src/pages/Router.js
@@ -4,36 +4,34 @@ import Login from "../components/auth/Login";
 import Signup from "../components/auth/Signup/index.jsx";
 import Splash from "../components/auth/Splash";
 // import Test from "../components/test";
-import Home from "../components/board/index.jsx";
+import Home from "../components/Board/index.jsx";
 // import Detail from "../components/board/detail/index";
 // import Write from "../components/board/write/index";
-import { useRecoilState , useRecoilValue } from "recoil";
+import { useRecoilState, useRecoilValue } from "recoil";
 // import { useUser } from "../hooks/useUser";
 import { userState } from "../atoms/atoms";
 function Router() {
   const userToken = localStorage.getItem("UserId");
-  const  userStates  = useRecoilValue(userState)
-    // const isAuthorized = sessionStorage.getItem("isAuthorized");
-    if (!userToken)
-      return (
-          <Routes>
-            <Route path="/auth/signup"  element={<Signup/>}/>
-            <Route path="/auth/login"  element={<Login/>}/>
-            <Route path="/" element={<Splash/>}/>
-            {/* <Route path="/main" element={<Home/>}/> */}
-          </Routes>
-      );
-  
+  const userStates = useRecoilValue(userState);
+  // const isAuthorized = sessionStorage.getItem("isAuthorized");
+  if (!userToken)
     return (
       <Routes>
-            <Route path="/" element={<Splash/>}/>
-            <Route path="/auth/signup"  element={<Signup/>}/>
-            <Route path="/auth/login"  element={<Login/>}/>
-          <Route path="/home" element={<Home/>}/>
-          {/* <Route path="/main/:userPk/:postPk" element={<Detail/>}/> */}
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/" element={<Splash />} />
+        {/* <Route path="/main" element={<Home/>}/> */}
+      </Routes>
+    );
 
+  return (
+    <Routes>
+      <Route path="/" element={<Splash />} />
+      <Route path="/auth/signup" element={<Signup />} />
+      <Route path="/auth/login" element={<Login />} />
+      <Route path="/home" element={<Home />} />
+      {/* <Route path="/main/:userPk/:postPk" element={<Detail/>}/> */}
     </Routes>
-        
   );
 }
 export default Router;
