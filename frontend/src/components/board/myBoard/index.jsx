d9ea5bceb888effb8166b49e40606dc213e63a04
yilin-kim <irenek91@korea.ac.kr>
2022-01-16 03:44:38 +0900

    [feat]MyBoard-photoModal,ExBoard-axios

--- frontend/src/components/board/myBoard/index.jsx
+++ frontend/src/components/board/myBoard/index.jsx
@@ -1,55 +1,62 @@
-import React,{  useEffect,useState } from "react";
+import React, { useEffect, useState } from "react";
 
-import Img from "../../../assets/sampleImg.png";
 import PhotoModal from "../PhotoModal";
 import * as S from "./style";
 import axios from "axios";
 import config from "../../../config/config";
+
 const MyBoard = (props) => {
-    const [photo, setPhoto] = useState();
-    const user_id = props.params.userPk
-    const userToken = localStorage.getItem("UserId");
-    const [isLoading, setIsLoading] = useState(true);
-    console.log("user", userToken)
-    const getPhoto = async () =>  {
-        await axios.get( config.BASE_URL+ "/pictures/" + user_id, {
-            headers:{ 'Authorization':`${userToken}`}
-        }
-        )
-    .then((res)=> {
-        console.log(res.data)
-         setPhoto(res.data);
+  const [showModal, setShowModal] = useState(false);
+  const [photo, setPhoto] = useState();
+  const user_id = props.params.userPk;
+  const userToken = localStorage.getItem("UserId");
+  const [isLoading, setIsLoading] = useState(true);
+  console.log("user", userToken);
+  const getPhoto = async () => {
+    await axios
+      .get(config.BASE_URL + "/pictures/" + user_id, {
+        headers: { Authorization: `${userToken}` },
       })
-     
-  }
-  useEffect(async() => {
+      .then((res) => {
+        console.log(res.data);
+        setPhoto(res.data);
+      });
+  };
+  const openModal = () => {
+    setShowModal(true);
+  };
+  const closeModal = () => {
+    setShowModal(false);
+  };
+  useEffect(async () => {
     await getPhoto();
     setIsLoading(false);
-}, []);
-  if(isLoading){
-    return <div>로딩중...</div>
-}
+  }, []);
+  if (isLoading) {
+    return <div>로딩중...</div>;
+  }
 
   return (
     <S.Container>
       {/* <S.Tab>내 사진첩</S.Tab> */}
-      {photo && photo.map(data => {
-                return (
-                <S.PostContainer>
-                    <S.ImageContainer>
-                      <img
-                        key={data.pictureid}
-                        src={data.image}
-                        alt={data.title}
-                        object-fit="cover"
-                        style={{ width: "100%" }}
-                      />
-                    </S.ImageContainer>
-                    <S.Title>{data.title}</S.Title>
-                  </S.PostContainer>
-                ) } ) }
-
-
+      {photo &&
+        photo.map((data) => {
+          return (
+            <S.PostContainer onClick={openModal}>
+              <S.ImageContainer>
+                <img
+                  key={data.pictureid}
+                  src={data.image}
+                  alt={data.title}
+                  object-fit="cover"
+                  style={{ width: "100%" }}
+                />
+              </S.ImageContainer>
+              <S.Title>{data.title}</S.Title>
+              <PhotoModal showModal={showModal} closeModal={closeModal} />
+            </S.PostContainer>
+          );
+        })}
     </S.Container>
   );
 };
