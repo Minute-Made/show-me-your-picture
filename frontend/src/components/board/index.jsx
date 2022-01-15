fffc5c2454f206a71bbda342d499df7721f8993f
Kim-Jiseong <jisungwise@naver.com>
2022-01-15 16:15:08 +0900

    홈 기틀 잡음

--- frontend/src/components/board/index.jsx
+++ frontend/src/components/board/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import profImgF from "../../assets/profIMG.svg"
 import { useRecoilValue, useRecoilState } from "recoil";
@@ -8,12 +8,26 @@ import * as S from "./style.js";
 import { userState } from "../../atoms/atoms.js";
 
 function Home() {
+    const [open, setOpen] = useState(false);
+    const onToggle = () => setOpen(!open);
   return(
     <>
+    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'flex-end'}}>
+        <S.Menu  onClick={onToggle} open={open}>
+            <span></span>
+            <span></span>
+            <span></span>
+        </S.Menu>
+    </div>
     <S.YBTOP></S.YBTOP>
     <S.TitleContainer>
-            <img src={profImgF} style={{width: '27%'}}></img>
-        <S.TitleWrapper></S.TitleWrapper>
+        <div style={{ width: '27%'}}>
+            <img src={profImgF} style={{width: '100%'}}></img>
+        </div>
+        <S.TitleWrapper>
+            <S.Title>김지성님의 흑역사진첩</S.Title>
+            <S.Info>이것은 매우 간단한 한줄 소개</S.Info>
+        </S.TitleWrapper>
     </S.TitleContainer>
     </>   
   )
