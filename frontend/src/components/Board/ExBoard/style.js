d7d1c70ddc5e5e798329c9dd762113da8176823f
yilin-kim <irenek91@korea.ac.kr>
2022-01-15 20:39:06 +0900

    교환하기 탭 기본틀

--- frontend/src/components/Board/ExBoard/style.js
+++ frontend/src/components/Board/ExBoard/style.js
@@ -11,17 +11,32 @@ export const Container = styled.div`
 
   display: flex;
   overflow: auto;
-  flex-direction: row;
-  align-items: space-between;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
 `;
 
 export const NotifContainer = styled.div`
   background-color: #ffffff;
   border: 0.5px solid #909090;
-  width: 90%;
-  height: 162px;
+  width: 100%;
+  height: 2rem;
+  margin: 0.3rem 0;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+`;
+
+export const NotifContent = styled.p`
+  font-family: "NeoDunggeunmo Pro";
+  font-size: 0.8rem;
+  margin: 0;
+`;
+
+export const NotifTime = styled.p`
+  font-family: "NeoDunggeunmo Pro";
+  font-size: 0.2rem;
+  color: #aaaaaa;
+  margin: 0;
 `;
