10e85343f9754a66a2ff41d38cafc4a46460cfdc
Kim-Jiseong <jisungwise@naver.com>
2022-01-15 15:24:53 +0900

    백업

--- frontend/src/components/board/index.jsx
+++ frontend/src/components/board/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import profImgF from "../../assets/profIMG.svg"
 import { useRecoilValue, useRecoilState } from "recoil";
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import * as S from "./style.js";
@@ -9,7 +10,11 @@ import { userState } from "../../atoms/atoms.js";
 function Home() {
   return(
     <>
-    <div></div>
+    <S.YBTOP></S.YBTOP>
+    <S.TitleContainer>
+            <img src={profImgF} style={{width: '27%'}}></img>
+        <S.TitleWrapper></S.TitleWrapper>
+    </S.TitleContainer>
     </>   
   )
 }
--- frontend/src/components/board/style.js
+++ frontend/src/components/board/style.js
@@ -10,3 +10,25 @@ export const Container = styled.div`
   flex-direction: column;
 
 `
+export const TopNavBar = styled.div`
+
+`
+export const Menu = styled.div`
+    
+`
+export const TitleContainer = styled.div`
+    width: 88%;
+    height: 120px;
+    background-color: #f4f4f4;
+    margin: 0 auto;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+    diplay:flex;
+    align-items: center;
+    justify-content: center;
+`
+export const TitleWrapper = styled.div`
+
+`
+export const YBTOP = styled.div`
+    height: 20px;
+`
