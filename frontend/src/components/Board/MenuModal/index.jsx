8d6d82fcfab128f7b5cee7f55f23cfa0fcc2c1ef
Kim-Jiseong <jisungwise@naver.com>
2022-01-15 18:41:44 +0900

    모달모달

---
+++ frontend/src/components/Board/MenuModal/index.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+// import modalBG from "../GlobalStyle"
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import * as S from "../style";
+// import { useUser } from "../../../hooks/useUser";
+// import { userState } from "../../atoms/atoms.js";
+
+function MenuModal(props) {
+    return(<>
+            {/* <modalBG menuModal={menuModal}></modalBG> */}
+    <S.BlackBG open={props.open}></S.BlackBG>
+    <S.MenuModal open={props.open}>
+        <S.MMMenu  open={props.open}>내 정보 수정</S.MMMenu>
+        <S.MMMenu  open={props.open}>나의 컬렉션</S.MMMenu>
+    </S.MenuModal>
+    </>)
+
+}
+export default MenuModal;
--- frontend/src/components/GlobalStyle.js
+++ frontend/src/components/GlobalStyle.js
@@ -1,4 +1,4 @@
-import styled, { createGlobalStyle } from "styled-components";
+import styled, { css, createGlobalStyle } from "styled-components";
 
 const GlobalStyleWrapper = createGlobalStyle`
   *, *::before, *::after {
@@ -15,10 +15,24 @@ const GlobalStyleWrapper = createGlobalStyle`
     margin: 0;
   }
 `;
-
+export const BlackBG= styled.div`
+${props =>
+    props.menuModal &&
+    css`
+    height: 100%;
+    width: 100%;
+    background-color: rgba(34, 34, 34, 0.7);
+    // position:fixed;
+    position: fixed;
+    transition: all .4s;
+    transform:translateX()
+    `}
+`
 const Container = styled.div`
   position: relative;
   background-color: #e6e9ec;
+  display:flex;
+  justify-content: center;
   // min-height: 100vh;
   // @media (min-width: 375px) {
     
@@ -27,7 +41,8 @@ const Container = styled.div`
 
 const Body = styled.div`
   max-width: 375px;
-  margin: 0 auto;
+  width: 100%;
+  // margin: 0 auto;
   background-color: #FFAB76;
   min-height: 100vh;
   // @media (min-width: 375px) {
