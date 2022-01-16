9c56fe5baab38b95633f3d22078b80bdd636bd7e
pyungjae <ppj050@snu.ac.kr>
2022-01-16 12:05:39 +0900

    trivial

---
+++ frontend/src/components/Board/index.jsx
@@ -0,0 +1,127 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import modalBG from "../GlobalStyle";
+import profImgF from "../../assets/profIMG.svg";
+import MenuModal from "./MenuModal/index";
+import AddModal from "./AddModal/index";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { Link, useNavigate, useLocation, useParams } from "react-router-dom";
+import * as S from "./style.js";
+import MyBoard from "./MyBoard/index";
+// import { useUser } from "../../../hooks/useUser";
+import { userState } from "../../atoms/atoms.js";
+import ExBoard from "./ExBoard";
+import config from "../../config/config.js"
+
+function Home() {  
+  const history = useNavigate();
+  const params = useParams();
+
+  //   console.dir(params.userPk)
+//   const userToken = localStorage.getItem("UserId");
+
+  const [nickname, setNick] = useState(false);
+  const [gender, setGender] = useState(false);
+  const [description, setDesc] = useState(false);
+//   console.dir(params.userPk)
+  const getProfile = async () =>  {
+    await axios.get( config.BASE_URL+ "/accounts/" + params.userPk)
+  .then((res)=> {
+    console.log(res.data)
+    setNick(res.data.nickname)
+    setGender(res.data.gender)
+    setDesc(res.data.description)
+  })
+  }
+  useEffect(async() => {
+    await getProfile();
+}, []);
+
+  const [open, setOpen] = useState(false);
+  const [openBG, setOpenBG] = useState(false);
+  const [plusModal, setPlusModal] = useState(false);
+  const [tab, setTab] = useState("myBoard");
+
+  const changeTabMy = () => {
+    setTab("myBoard");
+  };
+  const changeTabEx = () => {
+    setTab("exBoard");
+  };
+  const onToggle = () => {
+    setOpen(!open);
+    setOpenBG(!openBG);
+  };
+  const plusToggle = () => {
+    setPlusModal(!plusModal);
+    setOpenBG(!openBG);
+  };
+  const logout = async () => {
+        localStorage.removeItem("UserId");
+        history({
+          pathname: '/',
+        })
+      // })
+  };
+  // useEffect(() => {
+  //   // if(userStates != "none"){
+  //   if(userToken == null){
+  //     // console.log(user_id)
+  //     history({
+  //       pathname: '/',
+  //     })
+  //   }
+  // })
+  return (
+    <div style={{ diplay: "relative" }}>
+      <S.BlackBG openBG={openBG}></S.BlackBG>
+
+      {plusModal && <AddModal params={params}></AddModal>}
+
+      <MenuModal open={open}></MenuModal>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <S.Menu onClick={onToggle} open={open}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </S.Menu>
+        <S.Logout onClick={logout}>Log Out</S.Logout>
+      </div>
+      <S.YBTOP></S.YBTOP>
+      <S.TitleContainer>
+        <div style={{ width: "27%" }}>
+          <img src={profImgF} style={{ width: "100%" }}></img>
+        </div>
+        <S.TitleWrapper>
+          <S.Title>{nickname}님의 흑역사진첩</S.Title>     
+          <S.Info>{description}</S.Info>
+        </S.TitleWrapper>
+      </S.TitleContainer>
+      <S.TabWrapper>
+        <S.MyTab onClick={changeTabMy} tab={tab}>
+          내 사진첩
+        </S.MyTab>
+        <S.ExTab onClick={changeTabEx} tab={tab}>
+          교환하기
+        </S.ExTab>
+      </S.TabWrapper>
+
+      {tab == "myBoard" && <MyBoard params={params} />}
+      {tab != "myBoard" && <ExBoard params={params} />}
+      {tab == "myBoard" && (
+        <S.FixedAlign plusModal={plusModal}>
+          <S.PlusButton onClick={plusToggle} plusModal={plusModal}>
+            <i className="fas fa-plus"></i>
+          </S.PlusButton>
+        </S.FixedAlign>
+      )}
+    </div>
+  );
+}
+export default Home;
---
+++ frontend/src/components/Board/style.js
@@ -0,0 +1,269 @@
+import styled, { css, keyframes} from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 375px;
+  padding: 0 30px 0 30px;
+  margin: 0 auto;
+  display:flex;
+  justify-content: center;
+  flex-direction: column;
+
+`
+export const TopNavBar = styled.div`
+
+`
+export const Menu = styled.div`
+    width: 25px;
+    height: 23px;
+    position: relative;
+    transition: all .4s;
+    box-sizing: border-box;
+    margin-left: 6%;
+    margin-top: 20px;
+    z-index: 11;
+    cursor: pointer;
+    &:hover span {
+        background-color: #fff;
+    }
+    span {
+        display: inline-block;
+        transition: all .4s;
+        box-sizing: border-box;
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 5px;
+        background-color: #575F67;
+        border-radius: 4px;
+    }
+    span:nth-of-type(1) {
+        top: 0;
+      }
+    span:nth-of-type(2) {
+        top: 9px;
+    }
+    span:nth-of-type(3) {
+        bottom: 0;
+    }
+    ${props =>
+        props.open &&
+        css`
+        span {
+            background-color:#ff8787; 
+        }
+        span:nth-of-type(1) {
+            -webkit-transform : translateY(20px) rotate(-315deg);
+            transform : translateY(9.5px) rotate(-315deg);
+          }
+          span:nth-of-type(2) {
+            opacity : 0;
+          }
+          span:nth-of-type(3) {
+            -webkit-transform : translateY(-20px) rotate(315deg);
+            transform : translateY(-9.5px) rotate(315deg);
+          }
+    `}
+`
+export const TitleContainer = styled.div`
+    width: 88%;
+    height: 120px;
+    background-color: #f4f4f4;
+    margin: 0 auto;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.25);
+    display:flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`
+export const TitleWrapper = styled.div`
+    width: 56%;
+    display: flex;
+    flex-direction: column;
+    margin-left: 10px;
+`
+export const Title = styled.div`
+    width: 100%;
+    font-size: 1.125rem;
+    font-family: 'NeoDunggeunmo Pro';
+    white-space:nowrap;
+    margin-bottom: 20px;
+`
+export const Info = styled.div`
+    width: 100%;
+    font-size: 0.75rem;
+    font-family: 'NeoDunggeunmo Pro';
+    color: #A7B0C0;
+`
+const opacity = keyframes`
+    0% {
+      opacity: 0;
+    //   transform: translateX(-50%);
+    }
+    15% {
+      opacity: 0;
+    }
+    100%{
+      opacity: 1;
+    //   transform: translateX(0);
+    }
+  `;
+export const MenuModal = styled.div`
+    display: flex;
+    // justify-content: center;
+    align-items: center;
+    z-index: 9;
+    flex-direction: column;
+    div {
+        font-size: 0.8rem;
+        font-family: 'NeoDunggeunmo Pro';
+    }
+${props =>
+        props.open &&
+        css`
+    background-color: #eee;
+    width: 180px;
+    height:100%;
+    position: absolute;
+    // left: 0;
+    `}
+    animation-duration: 0.8s;
+    animation-name: ${({ open }) => open && opacity};
+`
+export const BlackBG= styled.div`
+z-index: 6;
+${props =>
+    props.openBG &&
+    css`
+    height: 100%;
+    width: 100%;
+    background-color: rgba(34, 34, 34, 0.7);
+    position: absolute;
+    left:0;
+    `}
+`
+
+export const YBTOP = styled.div`
+    height: 20px;
+`
+export const PlusButton = styled.div`
+color:white;
+width: 3.75rem;
+height: 3.75rem;
+display: flex;
+border-radius: 60px;
+background:#8E806A;
+align-items: center;
+cursor: pointer;
+justify-content: center;
+font-size: 1.5rem;
+pointer-events: all;
+box-shadow: 2px 2px 3px #999;
+transition: all ease 1s;
+&:hover {
+  color:#8E806A;
+  background: #eee;
+  & i {
+    transform: rotate( 360deg );
+    transition: all ease 1s;
+    // color:red;
+  }
+}
+${props =>
+    props.plusModal &&
+    css`
+      background: #ff6b6b;
+      &:hover {
+        background: #ff8787;
+      }
+      &:active {
+        background: #fa5252;
+      }
+      transform:  translate(-0%, 50%) rotate(45deg);
+    `}
+`;
+export const FixedAlign = styled.div`
+  width: 100%;
+  z-index:5;
+  max-width: 375px;
+  // height: 60px;
+  display: flex;
+  position: fixed;
+  margin: 0 auto;
+  position: fixed;
+  bottom: 10vh;
+  justify-content: center;
+  pointer-events: none;
+  transition: all ease 1s;
+  ${props =>
+    props.plusModal &&
+    css`
+      bottom:10vh;
+      z-index:30;
+    `}
+`
+export const MMMenu = styled.div`
+    display:none;
+    z-index: 10;
+  ${props =>
+    props.open &&
+    css`
+      display:flex;
+    `}
+`
+export const TabWrapper = styled.div`
+    display:flex;
+    width: 88%;
+    height: 42px;
+    justify-content: space-between;
+    margin: 0 auto;
+    padding-left: 10px;
+    padding-right: 10px;
+    margin-top: 20px;
+`
+export const MyTab = styled.div`
+    width: 40%;
+    height: 42px;
+    background-color: #909090;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 0.8rem;
+    box-shadow: 2px -2px 2px rgba(0, 0, 0, 0.25);
+    font-family: 'NeoDunggeunmo Pro';
+    cursor: pointer;
+    ${props =>
+    props.tab == 'myBoard' &&
+    css`
+      background-color: #f4f4f4;
+    `}
+`
+export const ExTab = styled.div`
+    cursor: pointer;
+    width: 40%;
+    height: 42px;
+    background-color: #909090;
+    display:flex;
+    align-items: center;
+    justify-content: center;
+    box-shadow: 2px -2px 2px rgba(0, 0, 0, 0.25);
+    font-size: 0.8rem;
+    font-family: 'NeoDunggeunmo Pro';
+    ${props =>
+    props.tab != 'myBoard' &&
+    css`
+      background-color:#f4f4f4 ;
+    `}
+`
+export const Logout = styled.div`
+  width: 20%;
+  height: 24px;
+  background-color: #FFFDA2;
+  display:flex;
+  align-items: center;
+  flex-direction: column;
+  cursor:pointer;
+  justify-content: flex-end;
+  padding: 4px;
+  margin-right: 6%;
+`;
